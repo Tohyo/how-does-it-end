b21cb27d30c228a487901fb31ad83350c7753bab
Tohyo <k.baujard@orange.fr>
2024-11-11 22:18:23 +0100

    remove unused file

--- frontend/test/helpers/api.ts
+++
@@ -1,9 +0,0 @@
-export const mockFetch = (data: any, options: { ok?: boolean; status?: number } = {}) => {
-  return jest.fn(() =>
-    Promise.resolve({
-      ok: options.ok ?? true,
-      status: options.status ?? 200,
-      json: () => Promise.resolve(data)
-    })
-  ) as jest.Mock;
-}; 
